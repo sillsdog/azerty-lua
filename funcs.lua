db641c4e7a31634c343b4d8f377eefe573b12f41
sills <32470786+sillsdog@users.noreply.github.com>
2018-09-03 13:48:17 -0400

    Create funcs.lua

---
+++ funcs.lua
@@ -0,0 +1,16 @@
+function wait(s)
+	local ntime = os.time() + s
+	repeat until os.time() > ntime
+end
+
+function SecondsToClock(seconds)
+	local seconds = tonumber(seconds)
+	if seconds <= 1 then
+		return "00:00:00";
+	else
+		hours = string.format("%02.f", math.floor(seconds/3600));
+		mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
+		secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
+		return hours..":"..mins..":"..secs
+	end
+end
